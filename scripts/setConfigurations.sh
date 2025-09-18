abef2dca4be8d2b9fa2c799d3bf1a3b6e14a3e4a
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2025-09-18 10:27:23 +0300

    Add scripts to get and set configuration of servers. On a long term, this should be done on a more reliable way

--- scripts/setConfigurations.sh
+++ scripts/setConfigurations.sh
@@ -8,13 +8,17 @@ PASS=$(yq .harborAdminPassword $DIRNAME/../values.yaml -r)
 
 AUTH=$(echo -n "$USER:$PASS" | base64)
 
-curl -X 'GET' \
+curl -X 'PUT' \
   'https://satama-test.csc.fi/api/v2.0/configurations' \
   -H 'Content-Type: application/json' \
-  -H "authorization: Basic $AUTH" | jq .
+  -H "authorization: Basic $AUTH" \
+  -w "satama-test: %{http_code}\n" \
+  -d@satama-test.oidc.yaml
 
-curl -X 'GET' \
+curl -X 'PUT' \
   'https://satama.csc.fi/api/v2.0/configurations' \
   -H 'Content-Type: application/json' \
-  -H "authorization: Basic $AUTH" | jq .
+  -H "authorization: Basic $AUTH" \
+  -w "satama: %{http_code}\n" \
+  -d@satama.oidc.yaml
 
